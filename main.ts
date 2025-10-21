c24dd7fc8e5a2acbace0adf2eadf9eb861a60cb9
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-10-21 17:00:10 -0400

    Make a traffic light using neopixels

--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack McNulty
+ * Created on: Oct 2025
+ * This program uses a neopixel to make a traffic light
 */
 
-basic.showString('Hello, World!')
+//variables
+let neopixelStrip: neopixel.Strip = null
+
+// setup 
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// turns lights on
+input.onButtonPressed(Button.A, function() {
+   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+   neopixelStrip.show()
+   pause(1000)
+
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.show()
+    pause(1000)
+   
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    pause(1000)
+
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    pause(1000)
+})
